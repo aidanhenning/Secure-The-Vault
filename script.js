32589f25a2a23aab2a57079854f4ce69924c0de2
aidanhenning <henningaidan@gmail.com>
2025-02-18 10:45:01 -0800

    First Push

---
+++ script.js
@@ -0,0 +1,13 @@
+// create a string for the user saying "you have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+
+let message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+// assign three variables for each number in the combination using 
+
+let numOne = 1 + 2 + 7;
+let numTwo = 4 * 10;
+let numThree = 78 / 2;
+
+// create an alert for the user combining the message followed by the numbers using temperate literals to make concise
+
+alert(`${message} ${numOne} - ${numTwo} - ${numThree}`);
